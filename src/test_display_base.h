b7d76f734843d89303b938e1402c3a9252a188ca
ein-shved <shved@lvk.cs.msu.su>
2013-03-27 18:05:36 +0400

    qxl-test: stable master branch
    Creating new branch

--- src/test_display_base.h
+++ src/test_display_base.h
@@ -86,7 +86,7 @@ typedef struct TestCommandSwitchSurface {
     uint32_t surface_id;
 } TestCommandSwitchSurface;
 
-typedef struct TestCommand {
+struct TestCommand {
     TestCommandType type;
     int times;  /* if =0 then infinitely
                  * else, command will removed after times execs
@@ -100,7 +100,7 @@ typedef struct TestCommand {
         TestCommandControl control;
         TestCommandSwitchSurface switch_surface;
     };
-} TestCommand;
+};
 
 typedef struct TestCommandItem TestCommandItem;
 
